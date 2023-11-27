7fc3a6c19d667b81218407995c20cd7ccd9cde68
joniikrz <jon.kryeziu@hotmail.com>
2023-11-27 19:18:16 +0100

    HOME
    Pa css

---
+++ index2.js
@@ -0,0 +1,15 @@
+home.onclick=() =>{
+    window.location.assign("index2.html")
+    }   
+    travel.onclick=() =>{
+        window.location.assign("index1.html")
+    }
+    travel1.onclick=() =>{
+        window.location.assign("index1.html")
+    }
+    ofertat.onclick=() =>{
+        window.location.assign("index3.html")
+    }
+    login.onclick=() =>{
+        window.location.assign("index4.html")
+    }
