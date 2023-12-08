a5f58817f1a15bc309c2c0c50d90f1c8db2f586b
Qendrimademii <149400724+Qendrimademii@users.noreply.github.com>
2023-12-08 22:41:15 +0100

    JS i Travel

---
+++ index1.js
@@ -0,0 +1,12 @@
+home.onclick=() =>{
+    window.location.assign("index2.html")
+    }  
+    travel.onclick=() =>{
+        window.location.assign("index1.html")
+    }
+    aboutus.onclick=() =>{
+        window.location.assign("index3.html")
+    }
+    login.onclick=() =>{
+        window.location.assign("index5.html")
+    }
