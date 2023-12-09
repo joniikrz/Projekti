517b11e552fa48352de524f22b16148701344728
joniikrz <jon.kryeziu@hotmail.com>
2023-12-09 17:17:54 +0100

    perfundimi log in
    index5

---
+++ index5.js
@@ -0,0 +1,24 @@
+let email=document.getElementById('email');
+let password=document.getElementById('password');
+let emailError=document.getElementById('emailError');
+let passwordError=document.getElementById('passwordError');
+let emailRegex=/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+let passwordRegex=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,30}$/;
+
+emailError.textContent='';
+passwordError.textContent='';
+function validate(){
+    if(!emailRegex.test(email.value)){
+        emailError.textContent='★Warning,Email eshte shkruar gabim!';
+        emailError.style.color="red";
+    }
+    passwordError.textContent='★Warning,Provoni nje password qe permban me shume se 8 karaktere!';
+    if(!passwordRegex.test(password.value)){
+        passwordError.style.color="red";
+        return;
+    }
+    alert('Jane ruajtuar te dhenat');
+}
+signup.onclick=()=>{
+    window.location.assign("index4.html");
+}
