53408a504722b26564d8bb7849c1e89127216727
Qendrimademii <149400724+Qendrimademii@users.noreply.github.com>
2024-01-25 15:43:12 +0100

    index6.php

---
+++ pages/index6.php
@@ -0,0 +1,241 @@
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TRAVEL</title>
+    <link rel="stylesheet" href="index6.css">
+</head>
+<body>
+    <div class="banner">
+    <div class="nav">
+        <img src="../assets/logoo.png" alt="" class="logo">
+        <ul>
+         <li><a href="../pages/index2.php">HOME</a></li>
+           <li><a href="../pages/index1.php">TRAVEL</a></li>
+           <li><a href="../pages/index3.php">ABOUT US</a></li>
+          <li><a href="../pages/index6.php">OFERTAT</a></li>
+          <li>LOG OUT</li>
+          
+        </ul>
+    </div>
+    </div>
+    <p class="ttll">OFERTAT</p>
+    <section class="vendet">
+    <div class="shtetet">
+        <div class="shteti">
+            <div class="zm">
+                <div class="fotoja">
+                    <img
+                        src="../assets/Selanik.jpg">
+                </div>
+            </div>
+
+            <div class="text">
+    
+                <h2>Selanik</h2>
+                <p class="cmimi">80$ / Për Person</p>
+                <div class="info-box">
+                    <p class="koha">🕓 2 Ditë</p>
+                    <p class="lokacioni">Nisja prej Prishtinë 05:00 Kthimi prej Selaniku 18:00</p>
+                </div>
+            </div>
+
+        </div>
+        <div class="shteti">
+            <div class="zm">
+                <div class="fotoja">
+                    <img src="../assets/Shkup.jpg">
+                </div>
+            </div>
+
+            <div class="text">
+                <h2>Shkup</h2>
+                <p class="cmimi">120$ / Për Person</p>
+                <div class="info-box">
+                    <p class="koha">🕓 3 Ditë</p>
+                    <p class="lokacioni">Nisja prej Prishtinë 06:00 Kthimi prej Shkupi 17:00</p>
+                </div>
+            </div>
+
+        </div>
+        <div class="shteti">
+            <div class="zm">
+                <div class="fotoja">
+                    <img src="../assets/Ulqin.jpg">
+                </div>
+            </div>
+
+            <div class="text">
+                <h2>Ulqin</h2>
+                <p class="cmimi">180$ / Për Person</p>
+                <div class="info-box">
+                    <p class="koha">🕓 4Ditë</p>
+                    <p class="lokacioni">Nisja prej Prishtinës 23:00 Kthimi prej Ulqini 16:00</p>
+                </div>
+            </div>
+
+        </div>
+        <div class="shteti">
+            <div class="zm">
+                <div class="fotoja">
+                    <img
+                        src="../assets/Sofia.jpg">
+                </div>
+            </div>
+
+            <div class="text">
+                <h2>Sofia</h2>
+                <p class="cmimi">$220 / Për Person</p>
+                <div class="info-box">
+                    <p class="koha">🕓 3 Ditë</p>
+                    <p class="lokacioni">Nisja prej ✈ Prishtinë</p>
+                </div>
+            </div>
+
+        </div>
+        <div class="shteti">
+            <div class="zm">
+                <div class="fotoja">
+                    <img src="../assets/Malta.jpg">
+                </div>
+            </div>
+
+            <div class="text">
+                <h2>Malta</h2>
+                <p class="cmimi">$300/ Për Person</p>
+                <div class="info-box">
+                    <p class="koha">🕓 5 Ditë</p>
+                    <p class="lokacioni">Nisja prej ✈ Tiranës</p>
+                </div>
+            </div>
+
+        </div>
+        <div class="shteti">
+            <div class="zm">
+                <div class="fotoja">
+                    <img src="../assets/Zagreb.jpg">
+                </div>
+            </div>
+
+            <div class="text">
+                <h2>Zagreb</h2>
+                <p class="cmimi">280$/ Për Person</p>
+                <div class="info-box">
+                <p class="koha">🕓 2 Ditë</p>
+                    <p class="lokacioni">Nisja prej  ✈  Tiranës </p>
+                </div>
+            </div>
+        </div>
+        <div class="shteti">
+          <div class="zm">
+              <div class="fotoja">
+                  <img src="../assets/Wien.jpg">
+              </div>
+          </div>
+
+          <div class="text">
+              <h2>Wien</h2>
+              <p class="cmimi">680$/ Për Person</p>
+              <div class="info-box">
+                  <p class="koha">🕓 6 Ditë</p>
+                  <p class="lokacioni">Nisja prej ✈ Shkupi</p>
+              </div>
+           </div>
+        </div>
+        <div class="shteti">
+            <div class="zm">
+                <div class="fotoja">
+                    <img src="../assets/Barcelona.jpg">
+                </div>
+            </div>
+           
+
+            <div class="text">
+                <h2>Barcelona</h2>
+                <p class="cmimi">780$/ Për Person</p>
+                <div class="info-box">
+                    <p class="koha">🕓 5 Ditë</p>
+                    <p class="lokacioni">Nisja prej ✈ Tiranës</p>
+                </div>
+            </div>
+            </div>
+            <div class="shteti">
+                <div class="zm">
+                    <div class="fotoja">
+                        <img src="../assets/Budapest.jpg">
+                    </div>
+                </div>
+               
+   
+                <div class="text">
+                    <h2>Budapest</h2>
+                    <p class="cmimi">450$/ Për Person</p>
+                    <div class="info-box">
+                        <p class="koha">🕓 5 Ditë</p>
+                        <p class="lokacioni">Nisja prej  ✈  Prishtines </p>
+                    </div>
+                </div>
+                </div>
+                <div class="shteti">
+                    <div class="zm">
+                        <div class="fotoja">
+                            <img src="../assets/Bruksel.jpg">
+                        </div>
+                    </div>
+                   
+       
+                    <div class="text">
+                        <h2>Bruksel</h2>
+                        <p class="cmimi">370$/ Për Person</p>
+                        <div class="info-box">
+                            <p class="koha">🕓 3 Ditë</p>
+                            <p class="lokacioni">Nisja prej ✈ Prishtinës</p>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="shteti">
+                        <div class="zm">
+                            <div class="fotoja">
+                                <img src="../assets/Amsterdam.jpg">
+                            </div>
+                        </div>
+                       
+           
+                        <div class="text">
+                            <h2>Amsterdam </h2>
+                            <p class="cmimi">200 $/ Për Person</p>
+                            <div class="info-box">
+                                <p class="koha">🕓 2 Ditë</p>
+                                <p class="lokacioni">Nisja prej ✈ Tiranës</p>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="shteti">
+                            <div class="zm">
+                                <div class="fotoja">
+                                    <img src="../assets/Luxemburg.jpg">
+                                </div>
+                            </div>
+                           
+               
+                            <div class="text">
+                                <h2>Luxemburg</h2>
+                                <p class="cmimi">385$/ Për Person</p>
+                                <div class="info-box">
+                                    <p class="koha">🕓 4 Ditë</p>
+                                    <p class="lokacioni">Nisja prej ✈Shkupi</p>
+                                </div>
+                            </div>
+                            </div>
+      </div>
+    </div>
+  </section>
+  <footer>
+    <div class="footer">
+        <h3>Copyrights &copy; 2023 | All rights reserved for <a href="#">JQ Travel - Prishtine</a></h3>
+    </div>
+  </footer>
+      
+  </body>
+  </html>  
